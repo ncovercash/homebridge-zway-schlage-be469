0a898fc314e1d6eeb97cfa1493a12291891ff0c3
Noah Overcash <me@ncovercash.dev>
2020-05-29 07:30:19 -0400

    Add test accessory creation

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,13 @@
 import axios, { Method as AxiosMethod, AxiosResponse } from "axios";
 import fs from "fs";
 import cookie from "cookie";
+import diff from "fast-array-diff";
 import {
 	API,
 	APIEvent,
+	Categories,
 	CharacteristicEventTypes,
+	CharacteristicGetCallback,
 	CharacteristicSetCallback,
 	CharacteristicValue,
 	DynamicPlatformPlugin,
@@ -14,6 +17,7 @@ import {
 	PlatformAccessoryEvent,
 	PlatformConfig,
 } from "homebridge";
+import { Be469Device, CommandClassIds, DataAPIReponse, ZWaySchlageBe469Config } from "./types";
 
 const PLUGIN_NAME = "homebridge-zway-schlage-be469";
 const PLATFORM_NAME = "schlage-be469";
@@ -28,17 +32,11 @@ export = (api: API) => {
 	api.registerPlatform(PLATFORM_NAME, ZWaySchlageBe469);
 };
 
-interface ZWaySchlageBe469Config extends PlatformConfig {
-	host: string;
-	user: string;
-	pass: string;
-}
-
 class ZWaySchlageBe469 implements DynamicPlatformPlugin {
 	protected readonly log: Logging;
 	protected readonly api: API;
 
-	protected readonly accessories: PlatformAccessory[] = [];
+	protected readonly accessories: Record<number, PlatformAccessory> = {};
 
 	protected config: {
 		host: string;
@@ -47,6 +45,7 @@ class ZWaySchlageBe469 implements DynamicPlatformPlugin {
 	};
 
 	protected session: string | null = null;
+	protected locks: Record<number, Be469Device> = {};
 
 	constructor(log: Logging, config: PlatformConfig, api: API) {
 		this.log = log;
@@ -66,10 +65,47 @@ class ZWaySchlageBe469 implements DynamicPlatformPlugin {
 		 * after this event was fired, in order to ensure they weren't added to homebridge already.
 		 * This event can also be used to start discovery of new accessories.
 		 */
-		api.on(APIEvent.DID_FINISH_LAUNCHING, () => {
-			log.info("Finished launchinggggggg");
+		api.on(APIEvent.DID_FINISH_LAUNCHING, async () => {
+			log.info("Finished launching");
 
-			this.initialContact();
+			await this.initialContact();
+
+			const delta = diff.diff(Object.keys(this.accessories), Object.keys(this.locks));
+
+			delta.added.forEach((nodeKey) => {
+				const nodeId = parseInt(nodeKey);
+
+				this.log("Creating accessory for lock #" + nodeId);
+
+				let name = this.locks[nodeId].lastState.data.givenName.value as string;
+				if (name == "") {
+					this.log.warn(
+						"This lock does not have a name set in Z-Way.  Defaulting to Allegion Keypad",
+					);
+					name = "Allegion Keypad";
+				}
+
+				const uuid = hap.uuid.generate(nodeKey);
+				const accessory = new Accessory(name, uuid, Categories.DOOR_LOCK);
+				accessory.context.nodeId = nodeId;
+
+				accessory.addService(hap.Service.LockMechanism, name);
+				accessory.addService(hap.Service.LockManagement, name);
+
+				this.configureAccessory(accessory);
+				this.api.registerPlatformAccessories(PLUGIN_NAME, PLATFORM_NAME, [accessory]);
+			});
+			delta.removed.forEach((nodeKey) => {
+				const nodeId = parseInt(nodeKey);
+
+				this.log("Lock #" + nodeId + " removed");
+
+				this.api.unregisterPlatformAccessories(PLUGIN_NAME, PLATFORM_NAME, [
+					this.accessories[nodeId],
+				]);
+
+				delete this.accessories[nodeId];
+			});
 		});
 	}
 
@@ -78,40 +114,132 @@ class ZWaySchlageBe469 implements DynamicPlatformPlugin {
 	 * It should be used to setup event handlers for characteristics and update respective values.
 	 */
 	configureAccessory(accessory: PlatformAccessory): void {
-		this.log("Configuring accessory %s", accessory.displayName);
+		this.log(
+			`Configuring accessory ${accessory.displayName} with node ID ${accessory.context.nodeId}`,
+		);
 
-		// accessory.on(PlatformAccessoryEvent.IDENTIFY, () => {
-		// 	this.log("%s identified!", accessory.displayName);
-		// });
+		accessory.on(PlatformAccessoryEvent.IDENTIFY, () => {
+			this.log(`#${accessory.context.nodeId} identified!`);
+		});
 
-		// accessory
-		// 	.getService(hap.Service.Lightbulb)!
-		// 	.getCharacteristic(hap.Characteristic.On)
-		// 	.on(
-		// 		CharacteristicEventTypes.SET,
-		// 		(value: CharacteristicValue, callback: CharacteristicSetCallback) => {
-		// 			this.log.info("%s Light was set to: " + value);
-		// 			callback();
-		// 		},
-		// 	);
+		const mechanismService = accessory.getService(hap.Service.LockMechanism)!;
+		mechanismService
+			.getCharacteristic(hap.Characteristic.LockCurrentState)
+			.on(CharacteristicEventTypes.GET, (callback: CharacteristicGetCallback) => {
+				this.log.info("Requested get for current");
+				callback(null, hap.Characteristic.LockTargetState.UNSECURED);
+			});
+		mechanismService
+			.getCharacteristic(hap.Characteristic.LockTargetState)
+			.on(CharacteristicEventTypes.GET, (callback: CharacteristicGetCallback) => {
+				this.log.info("Requested get for target");
+				callback(null, hap.Characteristic.LockTargetState.UNSECURED);
+			})
+			.on(
+				CharacteristicEventTypes.SET,
+				(value: CharacteristicValue, callback: CharacteristicSetCallback) => {
+					this.log.info("Requested set for target: " + value);
+					callback(null, hap.Characteristic.LockTargetState.UNSECURED);
+				},
+			);
 
-		this.accessories.push(accessory);
+		const managementService = accessory.getService(hap.Service.LockManagement)!;
+		managementService
+			.getCharacteristic(hap.Characteristic.LockControlPoint)
+			.on(
+				CharacteristicEventTypes.SET,
+				(value: CharacteristicValue, callback: CharacteristicSetCallback) => {
+					this.log.info("Tried writing to management service: " + value);
+					callback(new Error("Does nothing"));
+				},
+			);
+		managementService
+			.getCharacteristic(hap.Characteristic.LockControlPoint)
+			.on(
+				CharacteristicEventTypes.SET,
+				(value: CharacteristicValue, callback: CharacteristicSetCallback) => {
+					this.log.info("Tried writing to management service: " + value);
+					callback(new Error("Does nothing"));
+				},
+			);
+		managementService
+			.getCharacteristic(hap.Characteristic.Version)
+			.updateValue("1.0")
+			.on(CharacteristicEventTypes.GET, (callback: CharacteristicGetCallback) => {
+				callback(null, "1.0");
+			});
+
+		this.accessories[accessory.context.nodeId] = accessory;
 	}
 
 	// --------------------------- CUSTOM METHODS ---------------------------
 
 	protected async initialContact(): Promise<void> {
-		this.log("Sending initial request...");
-		const response = await this.makeRequest("GET", "Data/0", {}, "ZWaveAPI");
+		this.log("Sending initial request to enumerate devices...");
+		const response = await this.getData();
+
+		this.log(`Your controller appears to be a ${response.data.controller.data.vendor.value}!`);
+
+		Object.keys(response.data.devices).forEach((nodeId) => {
+			const device = response.data.devices[nodeId];
+			this.log(
+				`Found #${nodeId}:`,
+				`${device.data.givenName.value}`,
+				`(${device.data.vendorString.value}`,
+				`${device.data.deviceTypeString.value})`,
+			);
+
+			if (
+				device.data.vendorString.value == "Allegion" &&
+				device.data.deviceTypeString.value == "Secure Keypad"
+			) {
+				this.log("Identified this as a lock to be served by this platform");
+			} else {
+				return;
+			}
+
+			const lock: Be469Device = {
+				nodeId: parseInt(nodeId),
+				commandClasses: {
+					alarm: { instance: 0 },
+					battery: { instance: 0 },
+					doorLock: { instance: 0 },
+				},
+				lastState: device,
+			};
+
+			Object.keys(device.instances).forEach((instanceId) => {
+				const instance = device.instances[instanceId];
+				Object.keys(instance.commandClasses).forEach((commandClassId) => {
+					const commandClass = instance.commandClasses[commandClassId as CommandClassIds]!;
+					switch (commandClassId as CommandClassIds) {
+						case CommandClassIds.Alarm:
+							lock.commandClasses.alarm.instance = parseInt(instanceId);
+							this.log(`  -- Found relevant command class [${instanceId}] ${commandClass.name}`);
+							break;
+						case CommandClassIds.Battery:
+							lock.commandClasses.battery.instance = parseInt(instanceId);
+							this.log(`  -- Found relevant command class [${instanceId}] ${commandClass.name}`);
+							break;
+						case CommandClassIds.DoorLock:
+							lock.commandClasses.doorLock.instance = parseInt(instanceId);
+							this.log(`  -- Found relevant command class [${instanceId}] ${commandClass.name}`);
+							break;
+					}
+				});
+			});
+
+			this.locks[parseInt(nodeId)] = lock;
+		});
 	}
 
 	// eslint-disable-next-line
-	protected async makeRequest(
+	protected async makeRequest<T = any>(
 		method: AxiosMethod,
 		url: string,
 		data: Record<string, unknown> = {},
 		base = "ZAutomation/api/v1",
-	): Promise<AxiosResponse<any>> {
+	): Promise<AxiosResponse<T>> {
 		if (this.session == null) {
 			this.log("No requests have been made so far, looking for a session.");
 			try {
@@ -128,7 +256,7 @@ class ZWaySchlageBe469 implements DynamicPlatformPlugin {
 				this.log(`Got session ${session.substring(0, 6)}...`);
 				this.log("Testing access");
 
-				const response = await axios({
+				await axios({
 					method: "GET",
 					url: "status",
 					data: JSON.stringify({}),
@@ -163,14 +291,14 @@ class ZWaySchlageBe469 implements DynamicPlatformPlugin {
 				"Cookie": "ZWAYSession=" + this.session,
 				"ZWAYSession": this.session,
 			},
-			proxy: {
-				host: "127.0.0.1",
-				port: 8888,
-			},
 		});
 	}
 
-	async makeNewLoginSession(): Promise<void> {
+	protected getData(): Promise<AxiosResponse<DataAPIReponse>> {
+		return this.makeRequest<DataAPIReponse>("GET", "Data/0", {}, "ZWaveAPI");
+	}
+
+	protected async makeNewLoginSession(): Promise<void> {
 		this.log("No session exists or session is invalid.  Trying to login");
 		const response = await axios({
 			method: "POST",
