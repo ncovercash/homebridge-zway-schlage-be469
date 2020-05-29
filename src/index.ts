244956baff7a4004b1b4f392a7386660e7116fd6
Noah Overcash <me@ncovercash.dev>
2020-05-29 03:23:25 -0400

    Add login logic

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,6 @@
+import axios, { Method as AxiosMethod, AxiosResponse } from "axios";
+import fs from "fs";
+import cookie from "cookie";
 import {
 	API,
 	APIEvent,
@@ -43,7 +46,7 @@ class ZWaySchlageBe469 implements DynamicPlatformPlugin {
 		pass: string;
 	};
 
-	protected currentSession: string | null = null;
+	protected session: string | null = null;
 
 	constructor(log: Logging, config: PlatformConfig, api: API) {
 		this.log = log;
@@ -64,7 +67,7 @@ class ZWaySchlageBe469 implements DynamicPlatformPlugin {
 		 * This event can also be used to start discovery of new accessories.
 		 */
 		api.on(APIEvent.DID_FINISH_LAUNCHING, () => {
-			log.info("Finished launching");
+			log.info("Finished launchinggggggg");
 
 			this.initialContact();
 		});
@@ -97,7 +100,145 @@ class ZWaySchlageBe469 implements DynamicPlatformPlugin {
 
 	// --------------------------- CUSTOM METHODS ---------------------------
 
-	protected initialContact(): void {
-		// todo
+	protected async initialContact(): Promise<void> {
+		this.log("Sending initial request...");
+		const response = await this.makeRequest("GET", "Data/0", {}, "ZWaveAPI");
+	}
+
+	// eslint-disable-next-line
+	protected async makeRequest(
+		method: AxiosMethod,
+		url: string,
+		data: Record<string, unknown> = {},
+		base = "ZAutomation/api/v1",
+	): Promise<AxiosResponse<any>> {
+		if (this.session == null) {
+			this.log("No requests have been made so far, looking for a session.");
+			try {
+				const fileContents = fs.readFileSync(
+					this.api.user.storagePath() + "/." + PLUGIN_NAME + "-token",
+					{
+						encoding: "utf8",
+					},
+				);
+				const parsedFile = JSON.parse(fileContents);
+				const session = parsedFile.session;
+				this.session = session;
+
+				this.log(`Got session ${session.substring(0, 6)}...`);
+				this.log("Testing access");
+
+				const response = await axios({
+					method: "GET",
+					url: "status",
+					data: JSON.stringify({}),
+
+					baseURL: this.config.host + "ZAutomation/api/v1",
+					withCredentials: true,
+					responseType: "json",
+					headers: {
+						"Content-Type": "application/json",
+						"User-Agent": "Homebridge " + PLATFORM_NAME,
+						"Cookie": "ZWAYSession=" + session,
+						"ZWAYSession": session,
+					},
+				});
+
+				this.log("Success, session is valid!  Good to go!");
+			} catch (e) {
+				await this.makeNewLoginSession();
+			}
+		}
+		return await axios({
+			method: method,
+			url: url,
+			data: JSON.stringify(data),
+
+			baseURL: this.config.host + base,
+			withCredentials: true,
+			responseType: "json",
+			headers: {
+				"Content-Type": "application/json",
+				"User-Agent": "Homebridge " + PLATFORM_NAME,
+				"Cookie": "ZWAYSession=" + this.session,
+				"ZWAYSession": this.session,
+			},
+			proxy: {
+				host: "127.0.0.1",
+				port: 8888,
+			},
+		});
+	}
+
+	async makeNewLoginSession(): Promise<void> {
+		this.log("No session exists or session is invalid.  Trying to login");
+		const response = await axios({
+			method: "POST",
+			url: "login",
+			data: JSON.stringify({
+				login: this.config.user,
+				password: this.config.pass,
+			}),
+
+			baseURL: this.config.host + "ZAutomation/api/v1",
+			withCredentials: true,
+			responseType: "json",
+			validateStatus: () => true,
+			headers: {
+				"Content-Type": "application/json",
+				"User-Agent": "Homebridge " + PLATFORM_NAME,
+			},
+		});
+
+		this.log(`Got a response ${response.status} (${response.statusText})`);
+
+		if (response.status != 200) {
+			this.log("This appears to be an error.  Please check your login information.");
+			return;
+		}
+
+		this.log("Parsing cookie header(s)");
+		const parsedCookie = cookie.parse(response.headers["set-cookie"][0]);
+		const session = parsedCookie.ZWAYSession;
+		this.session = session;
+		const userId = response.data.data.id;
+
+		this.log(`Got session ${session.substr(0, 6)}..., saving`);
+		fs.writeFileSync(
+			this.api.user.storagePath() + "/." + PLUGIN_NAME + "-token",
+			JSON.stringify({
+				session,
+			}),
+		);
+
+		this.log("Trying to set session as non-expiring...");
+		const extendExpiryResponse = await axios({
+			method: "PUT",
+			url: "profiles/" + userId + "/token/" + session.substr(0, 6) + "...",
+			data: JSON.stringify({}),
+
+			baseURL: this.config.host + "ZAutomation/api/v1",
+			withCredentials: true,
+			responseType: "json",
+			validateStatus: () => true,
+			headers: {
+				"Content-Type": "application/json",
+				"User-Agent": "Homebridge " + PLATFORM_NAME,
+				"Cookie": "ZWAYSession=" + session,
+				"ZWAYSession": session,
+			},
+		});
+
+		if (extendExpiryResponse.status < 300) {
+			this.log("Success");
+			this.log(
+				"If you would like, you can remove your password from the config file (set it to an empty string)",
+			);
+		} else {
+			this.log("Unable to set as non-expiring.");
+			this.log(
+				"This platform may stop working or experience delays when needing to re-authenticate",
+			);
+		}
 	}
 }
